a57e66c291928d12509c8b54935e571b18d39670
Anup Kumangodan <anupkp@verizonmedia.com>
2021-04-13 16:33:01 -0700

    changes for passing secrets to exec via screwdriverr

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,67 @@
 plugins {
-   id("java")
+    `java-library`
     `maven-publish`
+    signing
 }
 
+group = "dev.yavin"
+version = "0.4"
+
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com")
-            credentials {
-                //username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = System.getenv("GH_TOKEN")
-                username = "anupkumangodan"
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "demo-config"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("yavin demo config files")
+                description.set("hjson files for demo")
+                url.set("http://yavin.dev")
+                licenses {
+                    license {
+                        name.set("MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jkusa")
+                        name.set("Jon Kilroy")
+                        email.set("kilroy@verizonmedia.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/yavin-dev/demo-config.git")
+                    developerConnection.set("scm:git:ssh:git@github.com:yavin-dev/demo-config.git")
+                    url.set("http://yavin.devs")
+                }
             }
         }
     }
-
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "yavin-dev.demo-config"
-            artifactId = "demo-config"
-            version = "0.1"
-
-            from(components["java"])
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/") //uri(layout.buildDirectory.dir("repos/releases"))
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") //uri(layout.buildDirectory.dir("repos/snapshots"))
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USER")
+                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_TOKEN")
+            }
         }
     }
 }
+
+signing {
+    project.setProperty("signing.gnupg.keyName", System.getenv("GPG_KEY"))
+    project.setProperty("signing.gnupg.passphrase", System.getenv("GPG_ENCPHRASE"))
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
+
